63a4c08123de07cfece4a575369d935a1ce2a8af
Mochamad Haikal <mochamadhaikal07@gmail.com>
2025-12-19 04:00:26 +0700

    Update by Haikal

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
     <script src="https://unpkg.com/feather-icons"></script>
 
     <!-- My Style -->
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <style>
       /* Login Page Specific Styles */
       .login-page {
