5f8ee3a0fa09ebc0a49c1c90d070cd68f354f9cb
Mochamad Haikal <mochamadhaikal07@gmail.com>
2025-12-22 22:02:54 +0700

    Upload routes Admin

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('app');
-})->name('login');
+})->name('home');
 
 Route::get('/home', function () {
     return view('home');
@@ -16,31 +15,15 @@ Route::get('/menu', function () {
     return view('menu');
 })->name('menu');
 
+
 Route::get('/admin', function () {
     return view('admin.admin');
-})->name('admin.dashboard');
-
-Route::get('/admin_menu', function () {
-    return view('admin.admin_menu');
-})->name('admin.menu');
-
-Route::get('/admin_transaksi', function () {
-    return view('admin.admin_transaksi');
-})->name('admin.transaksi');
-
-Route::get('/admin', function () {
-    return view('admin.admin', [
-        'totalMenu' => 0,
-        'todayOrders' => 0,
-        'completedOrders' => 0,
-        'pendingOrders' => 0,
-    ]);
-})->name('admin.dashboard');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+Route::get('/admin/menu', function () {
+    return view('admin.menu.admin_menu');
+});
+
+Route::get('/admin/transaksi', function () {
+    return view('admin.transaksi.admin_transaksi');
+});
